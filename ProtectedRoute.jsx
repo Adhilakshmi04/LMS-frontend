50cc1b956ac8ec99b6fde114c1c8f3c01f157911
Adhilakshmi04 <157974130+Adhilakshmi04@users.noreply.github.com>
2025-03-25 09:14:39 +0530

    Add files via upload
    Frontend components files added

---
+++ ProtectedRoute.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+
+const ProtectedRoute = ({ element, allowedRoles }) => {
+  const token = localStorage.getItem("token");
+  const sessionStart = localStorage.getItem("sessionStart"); 
+  const sessionDuration = 60*60*1000; 
+  const location = useLocation();
+  const isSessionValid = sessionStart && (Date.now() - parseInt(sessionStart) < sessionDuration);
+  
+  if (!token || !isSessionValid) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("sessionStart");
+    alert("Session expired. Please log in again.");
+    return <Navigate to="/" state={{ from: location }} />;
+  }
+  
+  const user = token ? JSON.parse(atob(token.split(".")[1])) : null;
+  
+  if (allowedRoles?.length > 0 && !allowedRoles.includes(user.role)) {
+    return <Navigate to="/" />;
+  }
+  
+  localStorage.setItem("sessionStart", Date.now().toString());
+  return element;
+};
+
+export default ProtectedRoute;
